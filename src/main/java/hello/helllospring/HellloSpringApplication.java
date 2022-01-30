a16174a0d70b2635216ebef9913133ab1122d708
jyyoun1022 <jyyoun1022@naver.com>
2022-01-30 17:13:53 +0900

    Initial commit

---
+++ src/main/java/hello/helllospring/HellloSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.helllospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellloSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/helllospring/HellloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.helllospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
